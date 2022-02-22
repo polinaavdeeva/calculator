b66b2c35c22972ee2d37195eb8208df82d79e94f
Polina Avdeeva <90614997+polinaavdeeva@users.noreply.github.com>
2022-02-22 03:08:29 +0300

    Add files via upload

---
+++ сalculator/сalculator/сalculator.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+using namespace std;
+#include <math.h>
+
+int main() 
+{
+    double firstNumber, secondNumber, resultOfCalculation;
+    char mathSign{};
+
+    cout << "Enter the first number for calculations: ";
+    cin >> firstNumber;
+
+    cout << "Enter the sign for the mathematical operation: ";
+    cin >> mathSign;
+
+    cout <<"Enter the second number for calculations: "; 
+    cin >> secondNumber;
+
+    switch (mathSign){
+    case '*':  
+       resultOfCalculation = firstNumber * secondNumber;
+       cout << "Result of multiplication: " << resultOfCalculation << endl;
+       break;
+    case '-' :
+        resultOfCalculation = firstNumber - secondNumber;
+        cout << "Result of subtracting numbers: " << resultOfCalculation << endl;
+        break;
+    case '+' : 
+        resultOfCalculation = firstNumber + secondNumber;
+        cout << "Result of adding numbers: " << resultOfCalculation << endl;
+        break;
+    case '/' : 
+        if (secondNumber != 0) {
+            resultOfCalculation = firstNumber / secondNumber;
+            cout << "Result of adding numbers: " << resultOfCalculation << endl;
+        }
+        else {
+            cout << "You cannot divide by zero, try again" << endl;
+        }
+        break;
+    default:
+        cout << "Please enter the correct mathematical sign."; 
+    }
+
+    return 0;
+
+
+}
